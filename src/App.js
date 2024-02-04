ec3f1f38b625694d454dac09d76a892f74da291b
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-02-04 20:28:38 +0900

    #7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import {useState ,useEffect} from "react";
 function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
+
+  const onDelete = (index) => {
+    setToDos((currentArray) => {
+      const updatedToDos = currentArray.filter((_, i) => i !== index);
+      return updatedToDos;
+    });
+  }
   const onChange = (event) => {setToDo(event.target.value)}
   const onSubmit = (event) => {
     event.preventDefault();
     if(toDo ===""){
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDos((currentArray) => [...currentArray, toDo ])
     setToDo("");
+    
   };
+
   console.log(toDos);
   return (
     <div>
@@ -21,6 +30,10 @@ function App() {
       <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
       <button>Add</button>
       </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => <li key={index}>{item} <button onClick={()=>onDelete(index)}>❌</button></li>)}
+      </ul>
     </div>
   );
 }
