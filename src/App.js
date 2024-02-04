474196371554faab0d9bc8dec8ac1b81f139ef0e
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-02-04 23:37:38 +0900

    Code Challenge

--- src/App.js
+++ src/App.js
@@ -2,40 +2,34 @@
 import {useState ,useEffect} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  const [getPrice, setGetPrice] = useState("");
+  const [myUsd, setMyUsd] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {setCoins(json);
+    setLoading(false)});
+  }, [])
+  const onChange = (event) => {setMyUsd(event.target.value) };
+  
+  const getCoinPrice= (event) => setGetPrice(event.target.value)
 
-  const onDelete = (index) => {
-    setToDos((currentArray) => {
-      const updatedToDos = currentArray.filter((_, i) => i !== index);
-      return updatedToDos;
-    });
-  }
-  const onChange = (event) => {setToDo(event.target.value)}
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo ===""){
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo ])
-    setToDo("");
-    
-  };
 
-  console.log(toDos);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
-      <button>Add</button>
-      </form>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input onChange={onChange} type="number" placeholder="How many USD?"></input>
+      <div></div>
+      {loading ? <strong>Loading...</strong>: <select onChange={getCoinPrice}><option>select the Coin</option>
+        {coins.map((coin) => <option value={coin.quotes.USD.price}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+      </select>
+      }
       <hr />
-      <ul>
-      {toDos.map((item, index) => <li key={index}>{item} <button onClick={()=>onDelete(index)}>❌</button></li>)}
-      </ul>
+      <div>{myUsd / getPrice}</div>
     </div>
+    
   );
-}
-
+};
 export default App;
