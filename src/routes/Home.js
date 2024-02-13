7fe26b6547a35745c71b90582ca1b7b176654061
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-02-13 15:59:05 +0900

    #7.5 React Router

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 
 import {useState ,useEffect} from "react";
-import Movie from "./components/Movie.js";
+import Movie from "../components/Movie.js";
 
 function Home() {
   const [loading, setLoading] = useState(true);
