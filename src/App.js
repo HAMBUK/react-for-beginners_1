4b7e275587cc796abe058354849661856a57ab35
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-02-13 14:29:06 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -3,33 +3,31 @@ import {useState ,useEffect} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  const [getPrice, setGetPrice] = useState("");
-  const [myUsd, setMyUsd] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (
+      await fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+    )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {setCoins(json);
-    setLoading(false)});
-  }, [])
-  const onChange = (event) => {setMyUsd(event.target.value) };
-  
-  const getCoinPrice= (event) => setGetPrice(event.target.value)
+    getMovies()
+  }, []);
+console.log(movies);
 
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input onChange={onChange} type="number" placeholder="How many USD?"></input>
-      <div></div>
-      {loading ? <strong>Loading...</strong>: <select onChange={getCoinPrice}><option>select the Coin</option>
-        {coins.map((coin) => <option value={coin.quotes.USD.price}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
-      </select>
-      }
-      <hr />
-      <div>{myUsd / getPrice}</div>
-    </div>
-    
-  );
+  return <div>
+    {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => (
+    <div key={movie.id}>
+      <img src={movie.medium_cover_image} />
+      <h2>{movie.title} ({movie.year})</h2>
+      <p>{movie.summary}</p>
+      <ul>
+        {movie.genres.map(g => <li key={g}>{g}</li>)}
+      </ul>
+      </div>))}</div>}
+  </div>
 };
 export default App;
